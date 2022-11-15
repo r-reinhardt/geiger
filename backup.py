09aa8d2a0247c6993e5fff216f74303cf76b6d48
r-reinhardt <rene.reinhardt.2003@gmail.com>
2022-11-15 10:23:54 +0100

    added debug mode

--- backup.py
+++ backup.py
@@ -3,11 +3,16 @@ import os
 
 ## config ##
 hours = 12
+debug = False
 ############
 
 # every x hours, print the current time
 while True:
+    if debug:
+        print('sleeping for ' + str(hours) + ' hours.')
     time.sleep(hours * 3600)
     if os.stat('/proc/uptime').st_mtime > hours * 3600:
         if debug:
+            print('system has been running for ' + str(hours + 3600) + '. backing up index.log.')
+        os.system("sudo cp /usr/local/ramdisk/geigerzaehler/index.log /home/pi/geigerzaehler/")
         
