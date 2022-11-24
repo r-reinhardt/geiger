d221e1f43ed242773db44d6abbece353b2af7e96
r-reinhardt <rene.reinhardt.2003@gmail.com>
2022-11-24 08:15:28 +0100

    fixed formatting

--- main.js
+++ main.js
@@ -5,29 +5,31 @@ let timestamp = document.querySelector('#timestamp > span');
 let updateInterval = 0.5; // in minuten
 
 async function updatePage() {
-	const log = await fetch('http://172.16.14.104:26662/api/latest').then((res) => res.json());
+  const log = await fetch('http://172.16.14.116:26662/api/latest').then((res) =>
+    res.json()
+  );
 
-	let lastValue = log.value;
-	let lastTime = log.timestamp;
+  let lastValue = log.value;
+  let lastTime = log.timestamp;
 
-	counter.innerHTML = lastValue;
+  counter.innerHTML = lastValue;
 
-	let green = 255 - red;
   let red = lastValue * 255 * 0.5;
+  let green = 255 - red;
 
-	wrapper.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
+  wrapper.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
 
-	timestamp.innerHTML = new Date(lastTime * 1000).toLocaleString('de-DE', {
-		year: 'numeric',
-		month: '2-digit',
-		day: '2-digit',
-		hour: '2-digit',
-		minute: '2-digit'
-	});
+  timestamp.innerHTML = new Date(lastTime * 1000).toLocaleString('de-DE', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 }
 
 updatePage();
 
 setInterval(async () => {
-	updatePage();
+  updatePage();
 }, 1000 * 60 * updateInterval);
